067b26f411a7858b204103c99470da8b2b842838
[kafunk] <kafunk@gmail.com>
2023-06-20 17:42:01 -0500

    resolve challenge 6

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 export default function WindowEvent () {
+
+  useEffect(() => {
+    const handleDblClick = () => alert("window event triggered");
+
+    // add event listener when component mounts (triggering this useEffect block)
+    window.addEventListener("dblclick", handleDblClick);
+
+    // return fn that will rm event listener when component is unmounted
+    return () => window.removeEventListener("dblclick", handleDblClick);
+  }, []);
+
   return (
     <h2>Window event active</h2>
   )
